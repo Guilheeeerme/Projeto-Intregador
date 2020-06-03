b522b18b8645c95abe3faecb2a0aff7dfb8ef9ad
Renato <natogfreitas@gmail.com>
2020-06-03 16:34:10 -0300

    tratativa de erros de videos e imagens

--- js/api.js
+++ js/api.js
@@ -1,31 +1,34 @@
 const baseUrl = 'https://api.themoviedb.org/3/';
 const apiKey = "d6ecb4865ebe46ec907e193a6b5c1c19";
 const sideNav = document.getElementById("side-nav");
+const generosList = document.getElementById("generos");
 const cards = document.querySelector(".cards");
 const titulo = document.getElementById("titulo");
 const descricao = document.getElementById("descricao");
-const background = document.getElementById("background");
+var background = document.getElementById("background");
 const linkYoutube = document.getElementById("link-youtube");
 const buttonModal = document.getElementById("button-modal");
 const iframeYoutube = document.querySelector(".modal-content");
-//https://api.themoviedb.org/3/discover/movie?api_key=###&with_genres=28
-const gen = document.getElementById("navbarSupportedContent");
+const button = document.getElementById("button");
+const search = document.getElementById("search");
 
-//genre/movie/list
+// //https://api.themoviedb.org/3/discover/movie?api_key=###&with_genres=28
+// //genre/movie/list
 async function searchGenres() {
     const url = `${baseUrl}genre/movie/list?api_key=${apiKey}&language=pt-BR`;
     const response = await fetch(url);
     const result = await response.json();
     const names = result.genres;
     names.forEach(element => {
-        sideNav.innerHTML +=
+
+        generosList.innerHTML +=
             `
                 <div class="generos-item">
-                    <span class="genrer" id="${element.id}" name="filmes" onclick="genresFilm(${element.id})">${element.name}</span>
+                    <span class="genrer" name="filmes" onclick="genresFilm(${element.id})">${element.name}</span>
                 </div>
             `
     });
-    sideNav.innerHTML += ` 
+    generosList.innerHTML += ` 
     <div class="generos-item">
         <span class="genrer" name="filmes" onclick="topFilms()">Top filmes do dia</span>
     </div>`
@@ -43,14 +46,12 @@ async function genresFilm(genero) {
     films.forEach(element => {
         const poster = element.poster_path;
         const backdrop = element.backdrop_path;
-        // element.overview = element.overview.replace(/\"/g, '');
-        // element.title = element.title.replace(/\"/g, '\\"');
         element.overview = element.overview.replace(/[\"']/g, "");
         if (backdrop !== null) {
             cards.innerHTML +=
                 `
                 <div class="card-item">
-                    <img src="https://image.tmdb.org/t/p/w300${poster}" alt="${element.title}" onclick="getBanner('${element.title}', '${element.overview}', '${backdrop}' ,'${element.id}')">
+                    <img src="https://image.tmdb.org/t/p/w300${poster}" alt="${element.title}" onclick="getBanner('${element.title}', '${element.overview}', '${backdrop}' ,'${element.id}', '${poster}')">
                 </div>
             `
         }
@@ -59,29 +60,45 @@ async function genresFilm(genero) {
 genresFilm(28);
 
 // movie{movie_id}/videos
-async function getBanner(title, overview, poster, idfilme) {
-
+async function getBanner(title, overview, poster, idfilme, posterCard) {
     const url = `${baseUrl}movie/${idfilme}/videos?api_key=${apiKey}`;
     const response = await fetch(url);
     const result = await response.json();
-    const youtube = result.results[0].key;
 
-    linkYoutube.innerHTML = `<a href="https://www.youtube.com/watch?v=${youtube}" target="_blank"><i class="fab fa-youtube fa-3x" id="youtube-icon"></i></a>`
+
+    if (overview == "") {
+        descricao.innerHTML = "Sem descrição";
+    }
+    else {
+        descricao.innerHTML = `<span class="descricao-filme">${overview}<span>`;
+    }
+
+    if (poster == "null") {
+        background.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${posterCard})`;
+    }
+    else {
+        background.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${poster})`;
+    }
     titulo.innerHTML = `<h1 clas="titulo-filme">${title}<h1>`;
-    descricao.innerHTML = `<span class="descricao-filme">${overview}<span>`;
-    background.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${poster})`;
     background.style.backgroundRepeat = "no-repeat";
     background.style.backgroundPosition = "center";
-
-    buttonModal.innerHTML =
-        `
+    if (result.results == 0) {
+        const youtube = result.results;
+        buttonModal.innerHTML = "";
+        iframeYoutube.innerHTML = "";
+        linkYoutube.innerHTML = "Video não encontrado";
+    } else {
+        const youtube = result.results[0].key;
+        linkYoutube.innerHTML = `<a href="https://www.youtube.com/watch?v=${youtube}" target="_blank"><i class="fab fa-youtube fa-3x" id="youtube-icon"></i></a>`
+        buttonModal.innerHTML =
+            `
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                 Ver vídeo 
             </button>
     
         `;
-    iframeYoutube.innerHTML =
-        `
+        iframeYoutube.innerHTML =
+            `
             <iframe 
                 src="https://www.youtube.com/embed/${youtube}"
                 frameborder="0"
@@ -89,6 +106,7 @@ async function getBanner(title, overview, poster, idfilme) {
                 allowfullscreen>
             </iframe>
         `
+    }
 
 }
 
@@ -99,16 +117,38 @@ async function topFilms() {
     const result = await response.json();
     const titles = result.results;
     titles.forEach(element => {
-        const backdrop = element.backdrop_path;
         element.overview = element.overview.replace(/[\"']/g, "");
         if (element.backdrop_path) {
             cards.innerHTML +=
                 `
                 <div class="card-item">
-                    <img src="https://image.tmdb.org/t/p/w300${element.poster_path}" alt="${element.title}" onclick="getBanner('${element.title}', '${element.overview}', '${backdrop}' ,'${element.id}')">
+                    <img src="https://image.tmdb.org/t/p/w300${element.poster_path}" alt="${element.title}" onclick="getBanner('${element.title}', '${element.overview}', '${element.backdrop_path}' ,'${element.id}')">
                 </div>
                `
         }
     });
 
 }
+
+async function procurarFilmes() {
+
+    cards.innerHTML = "";
+    const url = `${baseUrl}search/movie?query=${search.value}&api_key=${apiKey}&language=pt-BR`;
+    const response = await fetch(url);
+    const result = await response.json();
+    const filmSearch = result.results;
+    filmSearch.forEach(element => {
+        element.overview = element.overview.replace(/[\"']/g, "");
+        if (element.poster_path) {
+            cards.innerHTML +=
+                `
+            <div class="card-item">
+                <img src="https://image.tmdb.org/t/p/w300${element.poster_path}" alt="${element.title}" onclick="getBanner('${element.title}', '${element.overview}', '${element.backdrop_path}' ,'${element.id}', '${element.poster_path}')">
+            </div>
+       `
+        }
+    });
+    search.value = "";
+}
+button.addEventListener("click", procurarFilmes);
+
